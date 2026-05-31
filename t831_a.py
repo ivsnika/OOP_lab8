47ddf72367e95b36afa01a8751053029277f530d
ivsnika <v.s.ignatyeva@icloud.com>
2026-05-31 11:58:11 +0300

    Update t831_a.py

--- t831_a.py
+++ t831_a.py
@@ -1,18 +1,21 @@
 # t831_a.py
 
-
-def calculate_sequence_element(x, k):
-    if k < 1:
-        return "k повинно бути >= 1"
-    return (x**k) / k
+def sequence_generator(x):
+    k = 1
+    current = x
+    while True:
+        yield current
+        k += 1
+        current = current * x * (k - 1) / k
 
 
 if __name__ == "__main__":
-    try:
-        x = float(input("Введіть значення x: "))
-        k = int(input("Введіть значення k (k >= 1): "))
+    x = float(input("Введіть значення x: "))
+    k = int(input("Введіть значення k (k >= 1): "))
 
-        result = calculate_sequence_element(x, k)
+    if k >= 1:
+        gen = sequence_generator(x)
+        result = None
+        for _ in range(k):
+            result = next(gen)
         print(f"Результат x_{k} = {result}")
-    except ValueError:
-        print("Помилка: введіть коректні числа.")
