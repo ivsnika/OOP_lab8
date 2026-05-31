01fe6e0cc4942e14cc87eccff913c950dd265a2c
ivsnika <v.s.ignatyeva@icloud.com>
2026-05-31 11:58:54 +0300

    Update t831_b.py

--- t831_b.py
+++ t831_b.py
@@ -1,20 +1,18 @@
 # t831_b.py
 
 
-def calculate_product(n):
+def product_generator(n):
     product = 1.0
     for i in range(1, n + 1):
         product *= 1 / (i + 1)
-    return product
+        yield product
 
 
 if __name__ == "__main__":
-    try:
-        n = int(input("Введіть значення n (n >= 1): "))
-        if n < 1:
-            print("Параметр n має бути більшим або рівним 1.")
-        else:
-            result = calculate_product(n)
-            print(f"Результат P_{n} = {result}")
-    except ValueError:
-        print("Помилка: введіть ціле число.")
+    n = int(input("Введіть значення n (n >= 1): "))
+    if n >= 1:
+        gen = product_generator(n)
+        result = None
+        for val in gen:
+            result = val
+        print(f"Результат P_{n} = {result}")
