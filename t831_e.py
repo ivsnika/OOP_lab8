29e2e12f1b59a90e3a18523a299695875c3a1596
ivsnika <v.s.ignatyeva@icloud.com>
2026-05-31 12:01:07 +0300

    Update t831_e.py

--- t831_e.py
+++ t831_e.py
@@ -1,33 +1,32 @@
 # t831_e.py
 import math
 
-def taylor_sin(x, eps):
+
+def taylor_sin_generator(x):
     sum_sin = 0.0
     term = x
     n = 1
-
-    while abs(term) >= eps:
+    while True:
         sum_sin += term
+        yield sum_sin, term
         term = -term * (x**2) / ((2 * n) * (2 * n + 1))
         n += 1
 
-    return sum_sin
-
 
 if __name__ == "__main__":
-    try:
-        x = float(input("Введіть значення x (в радіанах): "))
-        eps = float(input("Введіть точність eps (наприклад, 0.00001): "))
+    x = float(input("Введіть значення x (в радіанах): "))
+    eps = float(input("Введіть точність eps (наприклад, 0.00001): "))
 
-        if eps <= 0:
-            print("Точність eps має бути більшою за 0.")
-        else:
-            taylor_res = taylor_sin(x, eps)
-            math_res = math.sin(x)
+    if eps > 0:
+        gen = taylor_sin_generator(x)
+        taylor_res = 0.0
 
-            print(f"\nРезультат через ряд Тейлора: {taylor_res}")
-            print(f"Результат з бібліотеки math:  {math_res}")
-            print(f"Різниця (абсолютна похибка):  {abs(taylor_res - math_res)}")
+        for current_sum, current_term in gen:
+            taylor_res = current_sum
+            if abs(current_term) < eps:
+                break
 
-    except ValueError:
-        print("Помилка: введіть коректні дійсні числа.")
+        math_res = math.sin(x)
+        print(f"\nРезультат через ряд Тейлора: {taylor_res}")
+        print(f"Результат з бібліотеки math:  {math_res}")
+        print(f"Різниця (абсолютна похибка):  {abs(taylor_res - math_res)}")
